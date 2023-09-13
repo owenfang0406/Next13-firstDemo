e03f43fe504373c8acf655ab7e888a690485f5cf
Owen Fang <owen.fang@technine.io>
2023-09-13 22:14:54 +0800

    first commit

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="p-5 bg-blue-500">
+      <p className="font-bold text-white">I am a Header</p>
+    </div>
+  );
+}
